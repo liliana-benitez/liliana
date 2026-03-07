0760d149209cd8c56cb588a14ccccc94858cfe62
Liliana Benitez <lilyybenitezz@gmail.com>
2026-03-07 16:14:16 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Experience from "./components/experience"
+import Intro from "./components/intro"
+import Navigation from "./components/nav"
+import Projects from "./components/projects"
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Intro />
+      <Experience />
+      <Projects />
+    </>
+  )
+}
+
+export default App
